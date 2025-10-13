b55c9063a7d4a6e68faa758f08f8af27d526997d
beliniko <niko.belikov@icloud.com>
2025-10-13 12:09:56 +0200

    added optional chaining for undefined

--- src/components/Exam/ExamDetail.tsx
+++ src/components/Exam/ExamDetail.tsx
@@ -29,63 +29,63 @@ const ExamDetail = ({ isVisible, setVisible, exam }: ExamDetailProps) => {
         <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, mt: 2 }}>
           <Box>
             <Typography level="title-sm">{t('pages.exams.addExam.fields.title')}:</Typography>
-            <Typography level="body-sm">{exam.title}</Typography>
+            <Typography level="body-sm">{exam?.title}</Typography>
           </Box>
 
           <Box>
             <Typography level="title-sm">{t('pages.exams.addExam.fields.moduleCode')}:</Typography>
-            <Typography level="body-sm">{exam.moduleCode}</Typography>
+            <Typography level="body-sm">{exam?.moduleCode}</Typography>
           </Box>
 
           <Box>
             <Typography level="title-sm">{t('pages.exams.addExam.fields.examDate')}:</Typography>
-            <Typography level="body-sm">{exam.examDate}</Typography>
+            <Typography level="body-sm">{exam?.examDate}</Typography>
           </Box>
 
           <Box>
             <Typography level="title-sm">{t('pages.exams.addExam.fields.room')}:</Typography>
-            <Typography level="body-sm">{exam.room}</Typography>
+            <Typography level="body-sm">{exam?.room}</Typography>
           </Box>
 
           <Box>
             <Typography level="title-sm">{t('pages.exams.addExam.fields.examType')}:</Typography>
-            <Typography level="body-sm">{exam.examType}</Typography>
+            <Typography level="body-sm">{exam?.examType}</Typography>
           </Box>
 
           <Box>
             <Typography level="title-sm">{t('pages.exams.addExam.fields.semester')}:</Typography>
-            <Typography level="body-sm">{exam.semester}</Typography>
+            <Typography level="body-sm">{exam?.semester}</Typography>
           </Box>
 
           <Box>
             <Typography level="title-sm">{t('pages.exams.addExam.fields.ects')}:</Typography>
-            <Typography level="body-sm">{exam.ects}</Typography>
+            <Typography level="body-sm">{exam?.ects}</Typography>
           </Box>
 
           <Box>
             <Typography level="title-sm">{t('pages.exams.addExam.fields.maxPoints')}:</Typography>
-            <Typography level="body-sm">{exam.maxPoints}</Typography>
+            <Typography level="body-sm">{exam?.maxPoints}</Typography>
           </Box>
 
           <Box>
             <Typography level="title-sm">{t('pages.exams.addExam.fields.duration')}:</Typography>
-            <Typography level="body-sm">{exam.duration} minutes</Typography>
+            <Typography level="body-sm">{exam?.duration} minutes</Typography>
           </Box>
 
           <Box>
             <Typography level="title-sm">{t('pages.exams.addExam.fields.attempt')}:</Typography>
-            <Typography level="body-sm">{exam.attemptNumber}</Typography>
+            <Typography level="body-sm">{exam?.attemptNumber}</Typography>
           </Box>
 
           <Box>
             <Typography level="title-sm">{t('pages.exams.addExam.fields.fileUploadRequired')}:</Typography>
-            <Typography level="body-sm">{exam.fileUploadRequired ? 'Yes' : 'No'}</Typography>
+            <Typography level="body-sm">{exam?.fileUploadRequired ? 'Yes' : 'No'}</Typography>
           </Box>
 
           <Box>
             <Typography level="title-sm">{t('pages.exams.addExam.fields.tools')}:</Typography>
             <Typography level="body-sm">
-              {exam.tools && exam.tools.length > 0 ? exam.tools.join(', ') : 'None'}
+              {exam?.tools && exam.tools.length > 0 ? exam?.tools.join(', ') : 'None'}
             </Typography>
           </Box>
         </Box>
